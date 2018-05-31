a8979e50aa39334a30a09b0e93268ce2e5337c49
Noor Khan <noor_khan@alumni.ubc.ca>
2018-05-31 00:20:36 -0700

    Updated

---
+++ Exercises/JavaScript Exercises/script.js
@@ -0,0 +1,8 @@
+console.log ("JS CONNECTED");
+
+var Fname = prompt("What's your first name ?");
+var Lname = prompt("What's your last name ?");
+var Age = prompt("What's your age ?");
+
+console.log ("Your full name is " + Fname + " " + Lname);
+console.log ("Your age is " + Age);
